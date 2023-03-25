fdde1ae6cda6b3c475041b3edc997e3d569f9b62
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-03-25 13:46:09 -0300

    First commit

---
+++ src/main/java/com/mcapecci/similarproducts/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.mcapecci.similarproducts.controller;
+
+import com.mcapecci.similarproducts.api.ProductApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController implements ProductApi {
+
+}
